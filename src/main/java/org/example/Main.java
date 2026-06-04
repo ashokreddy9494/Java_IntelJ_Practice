192048adbe03a27d92378e352ff0b5e0cbe99093
Ashok Vardhan Reddy <itreddyashokvardhanreddy@gmail.com>
2026-06-04 08:58:20 +0530

    Working hours

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 public class Main{
     public static void main(String[] args){
-        int a = 10;
-        int b  = 20;
-        System.out.println(a + b);
-
+        for(int i=1;i<=5;i++){
+            System.out.println("Day " + i);
+            for(int j=0;j<9;j++){
+                System.out.println("Working " + (j+8) + " - " + (j+9));
+            }
+        }
     }
 }
