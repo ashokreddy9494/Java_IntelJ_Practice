252ccd380199d34d85f8580c8f8fc70065d33531
Ashok Vardhan Reddy <itreddyashokvardhanreddy@gmail.com>
2026-06-09 10:26:24 +0530

    student details

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 package org.example;
-public class Main{
-    public static void main(String[] args){
-        int a = 10;
-        int b  = 20;
-        System.out.println(a + b);
+public class Main {
+    public static void main(String[] args) {
+         Student s1 = new Student();
+         s1.id = 10;
+        s1.age = 21;
+        s1.marks = 90;
+        s1.name = "Ashok";
+
+        System.out.println(s1.age);
+        System.out.println(s1.id);
+        System.out.println(s1.marks);
+        System.out.println(s1.name);
+
+        Student s2 = new Student();
+        s2.id = 1;
+        s2.age = 22;
+        s2.marks = 100;
+        s2.name = "Srikesh";
+        System.out.println(s2.name);
+        System.out.println(s1.age);
+        System.out.println(s1.id);
+        System.out.println(s1.marks);
+
+
+
 
     }
 }
+
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public  class Student {
+    static int id;
+    static int age;
+    static String name;
+    static int marks;
+    Student(){
+        System.out.println("Student is called ");
+
+    }
+
+}
